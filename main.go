f8ce235c121b9c30b3ef15c2d6209b87881018be
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2021-03-30 18:13:49 +0200

    Feature: Added Debug logging

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/adrianvillanueva997/GeofenceApi/pkg/api/config"
 )
 
+const DEBUGFILE = "logs/debug.log"
+
+func initDebugLogging() {
+	logFile, err := os.OpenFile(DEBUGFILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.SetOutput(logFile)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+}
+
 func main() {
+	initDebugLogging()
 	app := config.InitAPI()
 	err := app.Listen(":3000")
 	if err != nil {
